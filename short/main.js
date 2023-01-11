13a50acd1bc0bdff4025b34fff07e71f02cad522
lqinyli <lqinyli@139.com>
2023-01-12 01:44:32 +0800

    Update main.js

--- short/main.js
+++ short/main.js
@@ -17,14 +17,14 @@ let res
   .then(function(myJson) {
     res = myJson;
     document.getElementById("searchbtn").disabled=false;
-	document.getElementById("searchbtn").innerHTML=' Shorten it';
+	document.getElementById("searchbtn").innerHTML=' 点击生成';
     if(res.key!=="")
     document.getElementById("result").innerHTML=window.location.host+res.key;
     $('#exampleModal').modal('show')
   }).catch(function(err){alert("Unknow error. Please retry!");
   console.log(err);
   document.getElementById("searchbtn").disabled=false;
-	document.getElementById("searchbtn").innerHTML=' Shorten it';})
+	document.getElementById("searchbtn").innerHTML=' 点击生成';})
   }
   function copyurl (id, attr) {
     let target = null;
